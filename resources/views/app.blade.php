67d35f47d82170f02c672af0e13d75b3bb914bcb
PoweST2 <diegostd99@gmail.com>
2026-06-28 01:51:10 -0500

    cambio minimo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-title" content="Finanzas">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192.png">
+        <link rel="shortcut icon" href="/icons/icon-192.png">
         <link rel="manifest" href="/manifest.webmanifest">
         <link rel="apple-touch-icon" href="/icons/icon-192.png">
 
